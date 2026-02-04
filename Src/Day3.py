4e24ad261477b5faa87b1b95adf8e9af57e135ec
vrhadapad-git <vrhadapad@gmail.com>
2026-02-04 19:48:04 +0530

    Day 3

---
+++ Src/Day3.py
@@ -0,0 +1,18 @@
+# Task1 Lists
+inventory = ["Dates", "Apples", "Bananas", "Carrots"]
+print(inventory.append("Eggs"), inventory)
+print(inventory.remove("Bananas"), inventory)
+print(inventory[0:4])
+inventory.sort()
+print(inventory)
+
+# Task2 Tuple
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print(temperatures[0], temperatures[-1])
+print(temperatures[3:6])
+print(temperatures[-3:])
+# Task 3
+screen_res = (1920, 1080)
+print("The resolution is ", screen_res)
+#screen_res[0]=(1208)
+print("Tuples cannot be modified!")
