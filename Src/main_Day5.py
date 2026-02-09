bce1316e1b52b77dde65be59cbf8161192460918
vrhadapad-git <vrhadapad@gmail.com>
2026-02-09 19:51:33 +0530

    Day5Task_2

---
+++ Src/main_Day5.py
@@ -0,0 +1,3 @@
+import math_operations
+print(math_operations.average([10, 20, 30, 40]))
+print(math_operations.power(2,10))
---
+++ Src/math_operations.py
@@ -0,0 +1,7 @@
+def power(a, b):
+    return a**b
+
+def average(num):
+    t = sum(num)
+    l = len(num)
+    return t/l
